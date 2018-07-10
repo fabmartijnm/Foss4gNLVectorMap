1c9c113d9fbc62966ecfa4992cf6ba6cae0f9639
fabmartijnm <38882787+fabmartijnm@users.noreply.github.com>
2018-07-10 11:53:42 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+var map = new mapboxgl.Map({
+        container: 'map',
+        style: 'style.json',
+        hash: true,
+        zoom: 11,
+        pitch: 60,
+        bearing: 62.4,
+        center: [ 4.8, 52.4]
+    });
